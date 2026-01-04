299065a05743c38b94e7e3a2edd01fda922f665a
ryansaad <ryansaad85@gmail.com>
2026-01-04 09:54:13 -0500

    added files

--- DevOps-Project-19/tf-aws-eks/eks.tf
+++ DevOps-Project-19/tf-aws-eks/eks.tf
@@ -12,7 +12,25 @@ module "eks" {
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
 
+# This grants Admin permissions to the IAM User explicitly
+  enable_cluster_creator_admin_permissions = true
 
+  access_entries = {
+    # The key name can be anything, e.g., "jenkins_admin"
+    jenkins_admin = {
+      principal_arn = "arn:aws:iam::861276082757:user/Ryansaad_IAM_Admin"
+
+      policy_associations = {
+        admin = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          access_scope = {
+            type = "cluster"
+          }
+        }
+      }
+    }
+  }
+  # ============================
   eks_managed_node_groups = {
     nodes = {
       min_size     = 1
